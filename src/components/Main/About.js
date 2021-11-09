a6be33b4c6854061c76aecd9206a70e215717763
naffrey <nick.caffrey@icloud.com>
2021-11-08 18:21:20 -0600

    notated next steps on github in projects tab.

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About(props) {
     return (
         <div>
-            <h1>I am a full stack developer with skills in...</h1>
+            <h2>I am a full stack developer with skills in...</h2>
             <h3>HTML</h3>
             <h3>CSS</h3>
             <h3>JavaScript</h3>
@@ -14,7 +14,7 @@ function About(props) {
             <h3>Python</h3>
             <h3>Django</h3>
             <h3>Postgres</h3>
-            
+
         </div>
     );
 }
